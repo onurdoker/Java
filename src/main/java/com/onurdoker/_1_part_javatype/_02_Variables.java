db88d0a2f53a40204eee1c411e8230f077a1dc00
onurdoker <odoker@msn.com>
2025-02-16 23:40:56 +0300

    Data variables

--- src/main/java/com/onurdoker/_1_part_javatype/_02_Variables.java
+++ src/main/java/com/onurdoker/_1_part_javatype/_02_Variables.java
@@ -9,13 +9,15 @@ public class _02_Variables {
     /*
     Veri isimlendirmeleri:
         1. isim veya sıfat, zamir kullanmamız lazım.
+        Anlamlı ve açıklayıcı isimler seçiniz.
         2. _ veya $ ile başlayabilir.
         3. _ veya $ dışında özel karakter kullanılamaz.
         4. sayı ile bitebilir.
         5. sayı ile başlayamaz.
         6. değişken isimleri camelCase kuralına göre yazılır.
     */
-
+        int studentCounter = 10;
+        float totalSalary = 14.5355f;
         int schoolNumber = 25;
         System.out.println("schoolNumber: " + schoolNumber);
         schoolNumber = 44;
@@ -34,6 +36,14 @@ public class _02_Variables {
         // int schoolNumber = 11; // aynı isimde yazamazsınız.
         // int 44schoolNumber = 11; // sayı ile başlayamazsınız.
         // int ~schoolNumber = 11; // özel simge ile başlayamazsınız.
+        // int school-number = 11; // KebapCase kuralına göre yazamazsınız.
+        // int school number = 11; // Boşluk bırakamazsınız.
 
+        // YAZMAYALIM.
+        // float TotalSalary = 14.5355f; -> CamelCase kuralına uymuyor.
+
+        // SABIT
+        final int MAX_STUDENT = 100; // final ile sabit tanımlanır. Değeri değiştirilemez. JS'deki const gibi.
+        System.out.println("MAX_STUDENT: " + MAX_STUDENT);
     }
 }
